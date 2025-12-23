8a88f6a7b177915f21424a75c9c54dbef649e674
LordNana420 <nguzmanf@udistrital.edu.co>
2025-12-23 17:50:34 -0300

    Estructura inicial del proyecto FuegoVerde

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+require_once ("logica/Persona.php");
+require_once ("logica/Admin.php");
+require_once ("logica/EmpleadoInv.php");
+require_once ("logica/EmpleadoVend.php");
+
+if (isset($_GET["salir"])) {
+    session_unset();
+    session_destroy();
+}
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+<title>Fuego Verde</title>
+<link
+	href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+	rel="stylesheet">
+<link rel="stylesheet"
+	href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+<script
+	src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<script
+	src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js
+"></script>
+<script type="text/javascript"
+	src="https://www.gstatic.com/charts/loader.js"></script>
+
+</head>
+<?php
+if (! isset($_GET["pid"])) {
+    include ("presentacion/inicio.php");
+} else {
+    $pid = base64_decode($_GET["pid"]);
+    if (isset($_SESSION["id"])) {
+        include ($pid);
+    } else {
+        include ($pid);
+    }
+}
+?>
+
+
+</html>
